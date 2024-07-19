5d9be6e7a09543444053261f88636a83ea7cfe51
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2024-07-19 17:26:45 +0530

    initial commit

---
+++ project_api/account/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from account.models import Student
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+
+    # We are writing this becoz we need confirm password field in our Registratin Request
+    password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
+
+    class Meta:
+        model = Student
+        fields=['email','first_name','last_name','reg_no','mobile_number','password', 'password2']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+
+    # Validating Password and Confirm Password while Registration
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm Password doesn't match")
+        
+        return attrs
+    
+    def create(self, validate_data):
+        return Student.objects.create_user(**validate_data)
