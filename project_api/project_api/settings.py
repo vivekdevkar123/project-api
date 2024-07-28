67c3db472655efe989f38965138522ceef5831e6
vivekdevkar123 <mrvivekdevkar123@gmail.com>
2024-07-28 18:35:19 +0530

    send otp post api added

--- project_api/project_api/settings.py
+++ project_api/project_api/settings.py
@@ -82,25 +82,25 @@ WSGI_APPLICATION = 'project_api.wsgi.application'
 
 
 # Testing database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Production Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER':os.environ.get('DATABASE_USER'),
-        'PASSWORD':os.environ.get('DATABASE_PASSWORD'),
-        'HOST':os.environ.get('DATABASE_HOST'),
-        'PORT':os.environ.get('DATABASE_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DATABASE_NAME'),
+#         'USER':os.environ.get('DATABASE_USER'),
+#         'PASSWORD':os.environ.get('DATABASE_PASSWORD'),
+#         'HOST':os.environ.get('DATABASE_HOST'),
+#         'PORT':os.environ.get('DATABASE_PORT'),
+#     }
+# }
 
 
 # Password validation
