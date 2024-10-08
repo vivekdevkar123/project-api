3499ac2b358d434829caa6f132089c04bf0b95b3
Makarand-Tighare <makarand.tighare@gmail.com>
2024-10-08 17:38:09 +0530

    runned the server

--- project_api/manage.py
+++ project_api/manage.py
@@ -2,11 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
+from dotenv import load_dotenv
 
 def main():
-    dotenv.read_dotenv()
     """Run administrative tasks."""
+    load_dotenv() 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_api.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -17,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
